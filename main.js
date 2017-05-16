f936ea102e5218e76130a4e8e199fbee2cd3286d
chatwebrtc <chatwebrtc@mail85.com>
2017-05-16 10:25:49 +0800

    Ini

--- main.js
+++ main.js
@@ -13,25 +13,19 @@ var appid_text;
 var configuration={
     "iceServers": [
         {"url": "stun:cn1-stun.wilddog.com:3478"},
-        {"url":"stun:stun01.sipphone.com"},
-        {"url":"stun:stun.ekiga.net"},
-        {"url":"stun:stun.fwdnet.net"},
-        {"url":"stun:stun.ideasip.com"},
-        {"url":"stun:stun.iptel.org"},
-        {"url":"stun:stun.rixtelecom.se"},
-        {"url":"stun:stun.schlund.de"},
-        {"url":"stun:stun.l.google.com:19302"},
-        {"url":"stun:stun1.l.google.com:19302"},
-        {"url":"stun:stun2.l.google.com:19302"},
-        {"url":"stun:stun3.l.google.com:19302"},
-        {"url":"stun:stun4.l.google.com:19302"},
-        {"url":"stun:stunserver.org"},
-        {"url":"stun:stun.softjoys.com"},
-        {"url":"stun:stun.voiparound.com"},
-        {"url":"stun:stun.voipbuster.com"},
-        {"url":"stun:stun.voipstunt.com"},
-        {"url":"stun:stun.voxgratia.org"},
-        {"url":"stun:stun.xten.com"}        
+        {"url": "stun:stun.ekiga.net:3478"},
+        {"url": "stun:stun.ideasip.com:3478"},
+        {"url": "stun:stun.schlund.de:3478"},
+        {"url": "stun:stun.l.google.com:19302"},
+        {"url": "stun:stun1.l.google.com:19302"},
+        {"url": "stun:stun2.l.google.com:19302"},
+        {"url": "stun:stun3.l.google.com:19302"},
+        {"url": "stun:stun4.l.google.com:19302"},
+        {"url": "stun:stun.voiparound.com:3478"},
+        {"url": "stun:stun.voipbuster.com:3478"},
+        {"url": "stun:stun.voipstunt.com:3478"},
+        {"url": "stun:stun.voxgratia.org:3478"},
+        {"url": "stun:stun.xten.com:3478"}        
         
         
     ]
